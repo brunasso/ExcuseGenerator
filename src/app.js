ba5cb6dd53b1911ee3737c60e2111f2d99b2afb2
brunasso <53584556+brunasso@users.noreply.github.com>
2024-09-14 01:50:58 +0000

    Ultimas correciones

--- src/app.js
+++ src/app.js
@@ -7,21 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let WordsWho = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-  let WordsAction = ['ate', 'peed', 'crushed', 'broke'];
-  let WordsWhat = ['my homework', 'my phone', 'the car'];
-  let WordsWhen = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
-
+  let WordsWho = ["The dog", "My grandma", "The mailman", "My bird"];
+  let WordsAction = ["ate", "peed", "crushed", "broke"];
+  let WordsWhat = ["my homework", "my phone", "the car"];
+  let WordsWhen = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
 
   function generateExcuse() {
-      let Who = WordsWho[Math.floor(Math.random() * WordsWho.length)];
-      let Action = WordsAction[Math.floor(Math.random() * WordsAction.length)];
-      let What = WordsWhat[Math.floor(Math.random() * WordsWhat.length)];
-      let When = WordsWhen[Math.floor(Math.random() * WordsWhen.length)];
-      
-      return `${Who} ${Action} ${What} ${When}`;
+    let Who = WordsWho[Math.floor(Math.random() * WordsWho.length)];
+    let Action = WordsAction[Math.floor(Math.random() * WordsAction.length)];
+    let What = WordsWhat[Math.floor(Math.random() * WordsWhat.length)];
+    let When = WordsWhen[Math.floor(Math.random() * WordsWhen.length)];
+
+    return `${Who} ${Action} ${What} ${When}`;
   }
 
-  document.getElementById('excuse').innerHTML = generateExcuse();
-
+  document.getElementById("Excuse").innerHTML = generateExcuse();
 };
